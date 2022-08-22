6e3be97eff377c1d52b8fc174ce8ace82c354968
Geetesh Manchanda <manchanda8700geetesh@gmail.com>
2022-08-22 17:07:22 +0530

    Working fine

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Board from "./Components/Board";
+
+const app = () => {
+  return (
+    <>
+      <h1>TIC Tac Toe game </h1>
+      <Board />
+    </>
+  );
+};
+export default app;
