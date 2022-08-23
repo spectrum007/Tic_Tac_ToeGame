c6a0471741b43047ff08172122b13aaf69b42630
Geetesh Manchanda <manchanda8700geetesh@gmail.com>
2022-08-23 17:18:36 +0530

    did the initialy styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Board from "./Components/Board";
+import "./Styles/root.scss";
 
-const app = () => {
+const App = () => {
   return (
-    <>
+    <div className="app">
       <h1>TIC Tac Toe game </h1>
       <Board />
-    </>
+    </div>
   );
 };
-export default app;
+export default App;
--- src/Components/Square.jsx
+++ src/Components/Square.jsx
@@ -2,8 +2,11 @@ import React from "react";
 
 const Square = ({ value }) => {
   // porps is used to access the parametres of the components ......we can just destructure them and use the exact key word
-  console.log({ value });
-  return <button type="button"> {value} </button>;
+  return (
+    <button type="button" className="square">
+      {value}
+    </button>
+  );
 };
 
 export default Square;
