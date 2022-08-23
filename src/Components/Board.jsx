ae8596443326321b7a64a8d67362cc4b11b46dab
Geetesh Manchanda <manchanda8700geetesh@gmail.com>
2022-08-24 00:55:37 +0530

    Added state of board and events

--- src/Components/Board.jsx
+++ src/Components/Board.jsx
@@ -1,23 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import Square from "./Square";
 
 function Board() {
+  const [board, SetBoard] = useState(Array(9).fill(null));
+  const [isXnext, setIsXnext] = useState(false);
+  const handleSquareClick = (position) => {
+    if (board[position]) {
+      return;
+    }
+    SetBoard((prev) => {
+      return prev.map((square, pos) => {
+        if (pos === position) {
+          return isXnext ? "X" : "O";
+        }
+        return square;
+      });
+    });
+    setIsXnext((prev) => {
+      if (prev === false) {
+        return true;
+      }
+      return false;
+    });
+  };
+
+  const renderSquare = (position) => {
+    return (
+      <Square
+        value={board[position]}
+        onClick={() => {
+          handleSquareClick(position);
+        }}
+      />
+    );
+  };
   return (
     <div className="board">
       <div className="board-row">
-        <Square value={0} />
-        <Square value={1} />
-        <Square value={2} />
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
       <div className="board-row">
-        <Square value={3} />
-        <Square value={4} />
-        <Square value={5} />
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
       </div>
       <div className="board-row">
-        <Square value={6} />
-        <Square value={7} />
-        <Square value={8} />
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
       </div>
     </div>
   );
--- src/Components/Square.jsx
+++ src/Components/Square.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Square = ({ value }) => {
+const Square = ({ value, onClick }) => {
   // porps is used to access the parametres of the components ......we can just destructure them and use the exact key word
   return (
-    <button type="button" className="square">
+    <button type="button" className="square" onClick={onClick}>
       {value}
     </button>
   );
