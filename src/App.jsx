3397b36d7423e65b46e94f179f1b90556097572f
Geetesh Manchanda <manchanda8700geetesh@gmail.com>
2022-08-25 18:37:58 +0530

    Transform state to game history

--- src/App.jsx
+++ src/App.jsx
@@ -4,41 +4,49 @@ import { calculateWinner } from "./helper";
 import "./Styles/root.scss";
 
 const App = () => {
-  const [board, SetBoard] = useState(Array(9).fill(null));
-  const [isXnext, setIsXnext] = useState(false);
-  const winner = calculateWinner(board);
-  const CheckAllboxexareFilled = board.every((element) => element !== null);
+  const [history, Sethistory] = useState([
+    {
+      board: Array(9).fill(null),
+      isXnext: false,
+    },
+  ]);
+  console.log(history);
+  const [currentMove, SetcurrentMove] = useState(0);
+  const current = history[currentMove];
+
+  const winner = calculateWinner(current.board);
+  const CheckAllboxexareFilled = current.board.every(
+    (element) => element !== null
+  );
 
   const msg = winner
     ? `winner is ${winner}`
     : CheckAllboxexareFilled
     ? `Game Tie`
-    : `Next player is ${isXnext ? "X" : "O"}`;
+    : `Next player is ${current.isXnext ? "X" : "O"}`;
   const handleSquareClick = (position) => {
-    if (board[position] || winner) {
+    if (current.board[position] || winner) {
       return;
     }
-    SetBoard((prev) => {
-      return prev.map((square, pos) => {
+    Sethistory((prev) => {
+      const last = prev[prev.length - 1];
+
+      const newBaord = last.board.map((square, pos) => {
         if (pos === position) {
-          return isXnext ? "X" : "O";
+          return last.isXnext ? "X" : "O";
         }
         return square;
       });
+      return prev.concat({ board: newBaord, isXnext: !last.isXnext });
     });
-    setIsXnext((prev) => {
-      if (prev === false) {
-        return true;
-      }
-      return false;
-    });
+    SetcurrentMove((prev) => prev + 1);
   };
 
   return (
     <div className="app">
       <h1>Tic Tac Toe game </h1>
       <h2> {msg}</h2>
-      <Board board={board} handleSquareClick={handleSquareClick} />
+      <Board board={current.board} handleSquareClick={handleSquareClick} />
     </div>
   );
 };
